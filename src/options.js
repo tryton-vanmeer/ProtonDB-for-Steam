928343eeb6e672d199d57dd527b451e1ddf638a9
Tryton Van Meer <trytonvanmeer@gmail.com>
2018-10-01 23:23:54 -0400

    Added options.[html|js]

---
+++ src/options.js
@@ -0,0 +1,40 @@
+'use strict'
+
+var log = document.getElementById('status');
+
+function restore()
+{
+    browser.storage.local.get({
+        'new_tab': false
+    }, prefs => {
+        Object.keys(prefs).forEach(name => {
+            document.getElementById(name)[typeof prefs[name]
+                === 'boolean' ? 'checked' : 'value'] = prefs[name];
+        });
+    });
+}
+
+function save() {
+    const prefs = {
+        'new_tab': document.getElementById('new_tab').checked
+    };
+
+    browser.storage.local.set(prefs, () => {
+        log.textContent = 'Options saved.';
+        setTimeout(() => log.textContent = '', 750);
+        restore();
+    })
+}
+
+document.addEventListener('DOMContentLoaded', restore);
+document.getElementById('button_save').addEventListener('click', () => {
+    try
+    {
+        save();
+    }
+    catch (e)
+    {
+        log.textContent = e.message;
+        setTimeout(() => log.textContent = '', 750);
+    }
+});
