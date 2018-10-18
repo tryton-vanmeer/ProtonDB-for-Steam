0c77e18a5a5c3f962c9a22981906b363be105ddb
Tryton Van Meer <trytonvanmeer@gmail.com>
2018-10-17 22:33:29 -0400

    Moved insert rating function to steamapp.js

--- src/content/steamapp.js
+++ src/content/steamapp.js
@@ -1,5 +1,40 @@
 'use strict'
 
+// Insert the SPCR rating below DEVELOPER/PUBLISHER
+function insert_rating(rating) {
+    var container = document.createElement('div');
+
+    container.className = 'dev_row spcr_rating_row';
+    container.title = 'As seen by spcr.netlify.com';
+
+    var link = document.createElement('a');
+    link.className = 'spcr_rating_link spcr_rating_' + rating;
+
+    link.href = SPCR.HOMEPAGE + 'app/' + Steam.get_app_id(window.location.href);
+    link.textContent = rating;
+
+    var option_new_tab = chrome.storage.local.get('new_tab', result => {
+        if (result.new_tab)
+        {
+            link.target = '_blank'
+        }
+    });
+
+    var subtitle = document.createElement('div');
+    subtitle.className = 'subtitle column';
+    subtitle.textContent = 'SPCR Rating:';
+
+    container.appendChild(subtitle);
+    container.appendChild(link);
+
+    var element = document.querySelector('.user_reviews')
+
+    if (element)
+    {
+        element.append(container);
+    }
+}
+
 if (document.querySelector("span.platform_img.linux") === null) {
     var appid = Steam.get_app_id(window.location.href);
 
@@ -7,9 +42,9 @@ if (document.querySelector("span.platform_img.linux") === null) {
         if (request.readyState == 4) {
             if (request.status == 200) {
                 var json = JSON.parse(request.responseText)
-                Steam.store_insert_rating(json.tier);
+                insert_rating(json.tier);
             } else if (request.status == 404) {
-                Steam.store_insert_rating("Awaiting reports!");
+                insert_rating("Awaiting reports!");
             }
         }
     });
--- src/libspcr.js
+++ src/libspcr.js
@@ -7,41 +7,6 @@ class Steam {
 
         return parseInt(appid[2], 10);
     }
-
-    // Insert the SPCR rating below DEVELOPER/PUBLISHER
-    static store_insert_rating(rating) {
-        var container = document.createElement('div');
-
-        container.className = 'dev_row spcr_rating_row';
-        container.title = 'As seen by spcr.netlify.com';
-
-        var link = document.createElement('a');
-        link.className = 'spcr_rating_link spcr_rating_' + rating;
-
-        link.href = SPCR.HOMEPAGE + 'app/' + Steam.get_app_id(window.location.href);
-        link.textContent = rating;
-
-        var option_new_tab = chrome.storage.local.get('new_tab', result => {
-            if (result.new_tab)
-            {
-                link.target = '_blank'
-            }
-        });
-
-        var subtitle = document.createElement('div');
-        subtitle.className = 'subtitle column';
-        subtitle.textContent = 'SPCR Rating:';
-
-        container.appendChild(subtitle);
-        container.appendChild(link);
-
-        var element = document.querySelector('.user_reviews')
-
-        if (element)
-        {
-            element.append(container);
-        }
-    }
 }
 
 class SPCR {
