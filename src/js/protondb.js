1a66e331def03134137760df6d15c32ff79ac10f
chuck-flowers <chuck-flowers@users.noreply.github.com>
2021-02-14 11:49:45 -0500

    Adds support for ProtonDB ratings on a user's wishlist (#28)
    * Corrects the indentation of the manifest.json
    
    * Moves the 'game page specific' code into its own file
    
    * Adds support for adding ProtonDB ratings to a user's wishlist
    
    * Addresses feedback items
    
    - Properly supports the 'Native' label
    - Links to ProtonDB for games that are "Awaiting Reports!"
    - Removes an unused 'platform_icons' variable

--- src/js/protondb.js
+++ src/js/protondb.js
@@ -1,28 +1,5 @@
 "use strict";
 
-class Steam {
-    // Return a games appid from the url
-    static get_app_id(url) {
-        var appid = url.match(/\/(app)\/([0-9]{1,7})/);
-
-        return parseInt(appid[2], 10);
-    }
-
-    // Insert the ProtonDB rating below DEVELOPER/PUBLISHER
-    static insert_rating(rating, whitelisted = false) {
-        var element = document.querySelector(".user_reviews");
-        var subtitle = document.createElement("div");
-        subtitle.className = "subtitle column'";
-        subtitle.textContent = "ProtonDB Rating:";
-        var container = ProtonDB.get_rating_container(rating, whitelisted);
-        container.prepend(subtitle);
-    
-        if (element) {
-            element.append(container);
-        }
-    }
-}
-
 class ProtonDB {
     static get HOMEPAGE() {return "https://www.protondb.com/";}
     static get API_SUMMARY() {return "api/v1/reports/summaries/";}
@@ -46,13 +23,13 @@ class ProtonDB {
 
     static get_rating_container(rating, whitelisted = false) {
         var container = document.createElement("div");
-    
+
         container.className = "protondb_rating_row " + "steam_row";
         container.title = "As seen by the community of ProtonDB.com";
-    
+
         var link = document.createElement("a");
         link.className = "protondb_rating_link protondb_rating_" + rating;
-    
+
         link.href = ProtonDB.HOMEPAGE + "app/" + Steam.get_app_id(window.location.href);
         link.textContent = rating;
         link.target = "_blank"
@@ -65,23 +42,8 @@ class ProtonDB {
 
             link.appendChild(star);
         }
-    
+
         container.appendChild(link);
         return container;
     }
-}
-
-// Main
-var appid = Steam.get_app_id(window.location.href);
-
-if (document.querySelector("span.platform_img.linux") === null) {
-    ProtonDB.request_rating(appid, (rating) => {
-        if (rating == "pending") {
-            Steam.insert_rating("Awaiting reports!");
-        } else {
-            Steam.insert_rating(rating, whitelist.includes(appid) ? true : false);
-        }
-    });
-} else {
-    Steam.insert_rating("native");
 }
