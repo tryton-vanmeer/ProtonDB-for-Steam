4ddf69cba9cf383f71b028f2f574c98098041a76
Timothy Flowers <t_chuck_flowers@yahoo.com>
2021-02-13 23:02:47 -0500

    Addresses feedback items
    - Properly supports the 'Native' label
    - Links to ProtonDB for games that are "Awaiting Reports!"
    - Removes an unused 'platform_icons' variable

--- src/js/wishlist.js
+++ src/js/wishlist.js
@@ -1,19 +1,23 @@
 console.log('wishlist.js');
 
 class Wishlist {
-    static create_rating(rating, app_id, isWhitelisted) {
+    static create_rating(rating, app_id, isWhitelisted, isNative) {
         const ratingLink = document.createElement('a');
         ratingLink.classList.add('protondb_rating_link');
         ratingLink.style.verticalAlign = 'middle';
         
-        if (rating !== 'pending') {
+        if (isNative) {
+            ratingLink.classList.add('protondb_rating_native');
+            ratingLink.textContent = "Native";
+        } else if (rating !== 'pending') {
             ratingLink.classList.add('protondb_rating_' + rating);
-            ratingLink.href = ProtonDB.HOMEPAGE + "app/" + app_id;
             ratingLink.textContent = rating;
-            ratingLink.target = '_blank';
         } else {
             ratingLink.textContent = 'Awaiting Reports!';
         }
+
+        ratingLink.href = ProtonDB.HOMEPAGE + "app/" + app_id;
+        ratingLink.target = '_blank';
     
         if (isWhitelisted) {
             const star = document.createElement('span');
@@ -32,7 +36,6 @@ class Wishlist {
         for (var row of rows.getElementsByClassName('wishlist_row')) {
             const app_id = row.getAttribute('data-app-id');
             const stats_container = row.querySelector('.stats');
-            const platform_icons = row.getElementsByClassName('platform_icons').item(0);
     
             // If a protondb rating has already been loaded, skip to the next wishlist item.
             if (stats_container.querySelector('.protondb_rating_link')) {
@@ -42,7 +45,9 @@ class Wishlist {
             ProtonDB.request_rating(app_id, rating => {
                 console.log('Processing rating for ' + app_id + ' (' + rating + ')');
                 const isWhitelisted = whitelist.includes(app_id);
-                const rating_container = Wishlist.create_rating(rating, app_id, isWhitelisted);
+
+                const isNative = row.querySelector('.platform_img.linux') !== null;
+                const rating_container = Wishlist.create_rating(rating, app_id, isWhitelisted, isNative);
                 
                 const rating_field_label = document.createElement('div');
                 rating_field_label.classList.add('label');
