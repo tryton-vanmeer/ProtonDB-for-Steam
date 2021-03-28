1003b37fb18ad5d675a866e7f1bdcb3e50e43ab1
Alvaro Frias Garay <alvarofriasgaray@gmail.com>
2021-03-28 14:19:05 -0300

    Removed unused method

--- src/js/protondb.js
+++ src/js/protondb.js
@@ -4,15 +4,6 @@ class ProtonDB {
     static get HOMEPAGE() {return "https://www.protondb.com/";}
     static get API_SUMMARY() {return "api/v1/reports/summaries/";}
 
-    static request_summary(appid, callback) {
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function () {
-            callback(request);
-        }
-        request.open("GET", this.HOMEPAGE + this.API_SUMMARY + appid + ".json", true);
-        request.send(null);
-    }
-
     static request_rating(appid, callback)
     {
         chrome.runtime.sendMessage(
